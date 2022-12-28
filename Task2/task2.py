c87f2464b102cb7e0d25f309eb2d10087227dcd4
Khametov Bakir <bakir_90@inbox.ru>
2022-12-28 16:14:56 +0300

    add task2

---
+++ Task2/task2.py
@@ -0,0 +1,16 @@
+# Задайте последовательность чисел. 
+# Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+def listofnonrepeatingelements(change_list):
+    change_list = []
+    for i in original_list:
+        if i not in change_list:
+            change_list.append(i)
+    print(f"Список из неповторяющихся элементов: {change_list}")
+
+
+original_list = list(map(int, input("Введите числа через пробел:\n").split()))
+print(f"Оргинальный список: {original_list}")
+listofnonrepeatingelements(change_list=True)
+
+
